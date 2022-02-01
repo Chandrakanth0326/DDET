08c9cbfcea121d84606ed4610634d2baad56e04d
chandrakanth77 <cvantettinavar@gmail.com>
2022-02-01 12:38:14 +0530

    1st commit msg

---
+++ src/gitccc/sssss.java
@@ -0,0 +1,11 @@
+package gitccc;
+
+public class sssss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dsjkbms,bc,sb");
+		System.out.println(",mnbc,bn");
+	}
+
+}
